f6b4881a7a328cd435c0e1994183290466b1c8dc
Néstor Lara <nesla.92@hotmail.com>
2016-06-12 18:09:39 -0500

    Se integran los bindings del layout con butterknife y se agrega funcionalidad para que entregue el porcentaje de la propina

--- app/src/main/java/com/example/neslaram/tipcalc/MainActivity.java
+++ app/src/main/java/com/example/neslaram/tipcalc/MainActivity.java
@@ -1,26 +1,55 @@
 package com.example.neslaram.tipcalc;
 
-import android.app.Application;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.neslaram.tipcalc.application.TipCalcApplication;
+import com.example.neslaram.tipcalc.utils.Utils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+    private static final int TIP_STEP_CHANGE = 1;
+    private static final int DEFAULT_TIP_PERCENTAGE = 10;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.inputBill)
+    EditText inputBill;
+    @BindView(R.id.bttnCalculate)
+    Button bttnCalculate;
+    @BindView(R.id.inputPercentage)
+    EditText inputPercentage;
+    @BindView(R.id.bttnDecrease)
+    Button inputDecrease;
+    @BindView(R.id.bttnIncrease)
+    Button inputIncrease;
+    @BindView(R.id.bttnClear)
+    Button bttnClear;
+    @BindView(R.id.txtTip)
+    TextView txtTip;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
     }
@@ -47,12 +76,51 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @OnClick(R.id.bttnCalculate)
+    public void calculateOnClick() {
+        Utils.hideKeyboard(this);
+        String totalBill = inputBill.getText().toString().trim();
+        if (!totalBill.isEmpty()) {
+            double total = Double.parseDouble(totalBill);
+            int tipPercentage = getTipPercentage();
+            double tip = total * (tipPercentage / 100d);
+            String tipStr = String.format(getString(R.string.global_message_tip, tip));
+            txtTip.setVisibility(View.VISIBLE);
+            txtTip.setText(tipStr);
+        }
+    }
+
+    @OnClick(R.id.bttnDecrease)
+    public void decreaseOnClick() {
+        Utils.hideKeyboard(this);
+        int percentage = getTipPercentage();
+        if (percentage > 0) {
+            inputPercentage.setText(String.valueOf(percentage - 1));
+        }
+    }
+
+    @OnClick(R.id.bttnIncrease)
+    public void increaseOnClick() {
+        Utils.hideKeyboard(this);
+        inputPercentage.setText(String.valueOf(getTipPercentage() + 1));
+    }
+
     private void about() {
         TipCalcApplication application = (TipCalcApplication) getApplication();
-        String aboutUrl= application.getAboutUrl();
-        Intent intent= new Intent(Intent.ACTION_VIEW);
+        String aboutUrl = application.getAboutUrl();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(aboutUrl));
         startActivity(intent);
+    }
 
+    private int getTipPercentage() {
+        int tipPercentage = DEFAULT_TIP_PERCENTAGE;
+        String percentageStr = inputPercentage.getText().toString();
+        if (!percentageStr.isEmpty()) {
+            return Integer.parseInt(percentageStr);
+        } else {
+            inputPercentage.setText(String.valueOf(tipPercentage));
+            return tipPercentage;
+        }
     }
 }
