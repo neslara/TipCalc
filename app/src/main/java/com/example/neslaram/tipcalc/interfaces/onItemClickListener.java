775773d52ec4944f7a1975b480067a9297a9c96f
laran <nestor.lara@elnorte.com>
2016-06-14 12:23:49 -0500

    set wrap_content to recycler view

--- app/src/main/java/com/example/neslaram/tipcalc/interfaces/onItemClickListener.java
+++ app/src/main/java/com/example/neslaram/tipcalc/interfaces/onItemClickListener.java
@@ -2,9 +2,6 @@ package com.example.neslaram.tipcalc.interfaces;
 
 import com.example.neslaram.tipcalc.models.TipRecord;
 
-/**
- * Created by neslaram on 13/06/16.
- */
 public interface OnItemClickListener {
     void onItemClick(TipRecord record, int position);
 }
