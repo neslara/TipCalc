bd8ee07ae63bb287e7a61647c9d946a74c76272c
laran <nestor.lara@elnorte.com>
2016-06-14 12:46:15 -0500

    Mantiene estado de la primer actividad al regresar

--- app/src/main/java/com/example/neslaram/tipcalc/activities/TipDetailActivity.java
+++ app/src/main/java/com/example/neslaram/tipcalc/activities/TipDetailActivity.java
@@ -2,7 +2,9 @@ package com.example.neslaram.tipcalc.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.neslaram.tipcalc.R;
@@ -33,8 +35,22 @@ public class TipDetailActivity extends AppCompatActivity {
         String totalStr = String.format(getString(R.string.tipdetail_message_bill), intent.getDoubleExtra(BILL_TOTAL_KEY, 0));
         String tipStr = String.format(getString(R.string.tipdetail_message_bill), intent.getDoubleExtra(TIP_KEY, 0));
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         txtVwTimeStamp.setText(intent.getStringExtra(DATE_KEY));
         txtVwBillTotal.setText(totalStr);
         txtVwTip.setText(tipStr);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
